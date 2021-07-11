9fd57aaf89bb86515e3ef8b368f1b58ec127a3c0
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-11 14:09:31 +0300

    404 error handling route

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const app = express();
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorRoutes = require("./routes/error");
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(errorRoutes);
 
 app.listen(3000);
