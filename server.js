6075be96a038cd550b2a727003efea1bdea76bb1
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-11 17:13:17 +0300

    Added CSS

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const path = require("path");
+
 const app = express();
 
 const adminRoutes = require("./routes/admin");
@@ -8,6 +10,8 @@ const shopRoutes = require("./routes/shop");
 const errorRoutes = require("./routes/error");
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
