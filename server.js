44652247c70da623f0e746662a86b2f0ada32331
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-11 12:53:02 +0300

    Handling routes

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log("In the middleware!");
-    next();
+    res.send("<h1>Hello from Express Add Product middleware!</h1>")
 });
 
-
-app.use((req, res, next) => {
-    console.log("In another middleware!");
-    
+app.use('/', (req, res, next) => {
+    console.log("In the middleware!");
+    res.send("<h1>Hello from Express middleware!</h1>")
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
