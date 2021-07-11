2e4d67d23d45a6955847f82aef2d046ba8af7180
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-11 13:10:04 +0300

    More routes handling

--- server.js
+++ server.js
@@ -2,14 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-    console.log("In the middleware!");
-    res.send("<h1>Hello from Express Add Product middleware!</h1>")
+// app.use('/', (req, res, next) => {
+//     console.log("In the middleware!");
+//     res.send("<h1>Hello from Express middleware!</h1>")
+// });
+
+// app.use((req, res, next) => {
+//   console.log("Logging info");
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   next();
+// });
+
+// app.use((req, res, next) => {
+//   res.send("Sending a response to the client");
+// });
+
+app.use("/add-product", (req, res, next) => {
+  res.send("<h1>Hello from Express Add Product middleware!</h1>");
 });
 
-app.use('/', (req, res, next) => {
-    console.log("In the middleware!");
-    res.send("<h1>Hello from Express middleware!</h1>")
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Hello from the Users page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from the Index page</h1>");
 });
 
 app.listen(3000);
