818b5ced4b44d06f352eb1d834078e3da13a6baa
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-11 20:25:12 +0300

    Created a main layout for the templates

--- routes/error.js
+++ routes/error.js
@@ -7,6 +7,7 @@ const router = express.Router();
 
 router.use((req, res) => {
     res.status(404).render("404", {pageTitle: "Page Not Found"});
+    
 });
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const products = adminData.products;
-  res.render("index", {products, pageTitle: "My Products"});
+  res.render("index", {products, pageTitle: "My Products", path: "/"});
 });
 
 module.exports = router;
