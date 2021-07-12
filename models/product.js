4acb16e21828b65fdf856b06e81524600c077ca8
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-12 18:15:14 +0300

    Updated the add product form

--- models/product.js
+++ models/product.js
@@ -7,7 +7,7 @@ const p = path.join(
   "products.json"
 );
 
-const getProductsFromFile = cb => {
+const getProductsFromFile = (cb) => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       return cb([]);
@@ -17,12 +17,15 @@ const getProductsFromFile = cb => {
 };
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile(products => {
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
