064a1d904b36134417736a477f11bd5b51a0439d
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-12 17:50:46 +0300

    Added shop routes for cart and checkout

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,11 +4,32 @@ exports.getProducts = (req, res) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
       products: products,
-      pageTitle: "Shop",
-      path: "/",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true,
+      pageTitle: "All Products",
+      path: "/product-list"
     });
+  });
+};
+
+exports.getIndex = (req, res) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      products: products,
+      pageTitle: "Shop",
+      path: "/"
+    });
+  });
+};
+
+exports.getCart = (req, res) => {
+  res.render("shop/cart", {
+    pageTitle: "Your Cart",
+    path: "/cart"
+  });
+};
+
+exports.getCheckout = (req, res) => {
+  res.render("shop/checkout", {
+    pageTitle: "Checkout",
+    path: "/checkout"
   });
 };
