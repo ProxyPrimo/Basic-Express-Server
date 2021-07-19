4fa5d68dfe6ccc1a0d2c8eabb8fb39d55f53fd1b
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-19 13:18:34 +0300

    Setup a dummy User

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ const path = require("path");
 const app = express();
 
 const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -19,11 +32,27 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
 
-sequelize.sync().then(r => {
-    // console.log(r);
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
+sequelize
+  .sync({ force: true })
+  .then((r) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      User.create({
+        name: "Peter",
+        email: "peter@peter.com",
+      });
+    }
+    return Promise.resolve(user);
+  })
+  .then((user) => {
+    console.log(user);
     app.listen(3000);
-}).catch(e => {
+  })
+  .catch((e) => {
     console.log(e);
-});
-
-
+  });
