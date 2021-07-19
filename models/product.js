d9a4c10be4e66e7328f07faa0fb4190edf1793d9
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-19 11:26:32 +0300

    Created a Product model with Sequelize

--- models/product.js
+++ models/product.js
@@ -1,29 +1,28 @@
-const db = require("../util/database");
+const Sequelize = require("sequelize");
 
-const Cart = require("./cart");
+const sequelize = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
-
-  save() {
-    return db.execute(
-      "INSERT INTO products (title, price, image_url, description) VALUES (?, ?, ?, ?)",
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
-
-  static deleteById() {}
-  static fetchAll() {
-    return db.execute("SELECT * FROM products");
-  }
-
-  static getProductById(id) {
-    return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-  }
-};
+modules.export = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
