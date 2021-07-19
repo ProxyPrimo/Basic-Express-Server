ef3fdcc26cd431693d9f58af0f9eb93ae3227af6
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-19 14:39:17 +0300

    Updated Cart model and added Cart Item model

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
+// Adding express
 const express = require("express");
 
 const path = require("path");
 
 const app = express();
 
+// Database configurations
 const sequelize = require("./util/database");
+
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
+// View Engine
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Auto-generated User
 app.use((req, res, next) => {
   User.findByPk(1)
     .then((user) => {
@@ -24,6 +31,7 @@ app.use((req, res, next) => {
     });
 });
 
+// Routes
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorRoutes = require("./routes/error");
@@ -32,11 +40,19 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorRoutes);
 
+// Association
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
+User.hasMany(Product); // optional
+
+User.hasOne(Cart);
+Cart.belongsTo(User); // optional
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  .sync() // { force: true }
+  // .sync({force: true})
+  .sync()
   .then((r) => {
     return User.findByPk(1);
   })
